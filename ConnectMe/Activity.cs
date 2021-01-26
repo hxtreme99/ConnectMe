af7553228c4aa19b70f52fa8da767f9e09d6e642
cristovaosg <cristovaosg09@gmail.com>
2021-01-26 19:56:26 +0000

    refactor

--- ConnectMe/Activity.cs
+++ ConnectMe/Activity.cs
@@ -5,8 +5,7 @@ namespace ConnectMe
 {
     class Activity
     {
-        static readonly List<User> _users = new List<User>();
-
+        
         public Activity(int id,
             string name,
             User user,
@@ -25,15 +24,7 @@ namespace ConnectMe
             Description = description;
             MaxPeople = maxPeople;
         }
-        //private int id;
-        //private String name;
-        //private User user;
-        //private Category category;
-        //private String location;
-        //private String date;
-        //private String description;
-        //private int maxPeople;
-
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public User User { get; set; }
@@ -43,11 +34,6 @@ namespace ConnectMe
         public string Description { get; set; }
         public int MaxPeople { get; set; }
 
-        public static List<User> GetUsers() => _users;
-
-        public static void AddUser(User user)
-        {
-            _users.Add(user);
-        }
+        
     }
 }
--- ConnectMe/User.cs
+++ ConnectMe/User.cs
@@ -4,8 +4,6 @@ namespace ConnectMe
 {
     abstract class User
     {
-        static readonly List<User> _users = new List<User>();
-
         public User(int id, string name, string email, string username, string password)
         {
             Id = id;
@@ -21,11 +19,6 @@ namespace ConnectMe
         public string Username { get; set; }
         public string Password { get; set; }
 
-        public static List<User> GetUsers() => _users;
-
-        public static void AddUser(User user)
-        {
-            _users.Add(user);
-        }
+       
     }
 }
